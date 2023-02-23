4e888862819107ab7fb7e207038df8f2c0cd45db
The_Sheleng <sheleng55@gmail.com>
2023-02-23 17:23:09 +0200

    Add project files.

---
+++ 20.02.2023/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card
+{
+    internal class Card
+    {
+        public float Cash { get; private set; }
+        public int Number { get; private set; } 
+        public string BIO { get; private set; }
+        public DateTime Validity { get; private set; }
+        private int Pin { get; set; }
+        public static float CreditLimit;
+        static Card()
+        {
+            CreditLimit = float.MinValue;
+        }
+        public Card(float cash, int number, string bio, DateTime validity, int pin)
+        {
+            Cash = cash;
+            Number = number;
+            BIO = bio;
+            Validity = validity;
+            Pin = pin;
+        }
+        public delegate void Notify(string mess);
+        public event Notify OnNotify;
+        public void SetPin(int pin)
+        {
+            Pin = pin;
+            OnNotify.Invoke("PIN code has been changed");
+        }
+        public void TopUp(float cash)
+        {
+            Cash += cash;
+            OnNotify.Invoke($"{cash}$ credited to the account");
+        }
+        public void TakeOff(float cash)
+        {
+            Cash -= cash;
+            OnNotify.Invoke($"{cash} has been deducted");
+        }
+    }
+}
---
+++ 20.02.2023/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._02._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var card = new Card.Card(500, 123123123, "Test Testovich", new DateTime(2030, 12, 12), 1234);
+            card.OnNotify += s => Console.WriteLine(s);
+            card.SetPin(4321);
+            card.TakeOff(300);
+            card.TopUp(800);
+            Console.WriteLine($"Cash: {card.Cash}");
+        }
+    }
+}
